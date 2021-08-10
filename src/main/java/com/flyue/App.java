e28fea1ce34e099d6b55507b41fffcf83b450fde
Ferko <497625679@qq.com>
2021-08-10 09:57:00 +0800

    third commit

--- src/main/java/com/flyue/App.java
+++ src/main/java/com/flyue/App.java
@@ -8,7 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Git!" );
-        System.out.println( "Hello Git!" );
+        System.out.println( "Hello Git!1" );
+        System.out.println( "Hello Git!2" );
+        System.out.println( "Hello Git!3" );
     }
 }
